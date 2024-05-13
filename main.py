f26a5b78919a76e05e5469c77ac501885e1b13f8
Yuyul1004 <y.lin2349@gmail.com>
2024-05-13 10:58:47 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.display.set_caption("Dadish")
 clock = pygame.time.Clock()
 fps = 50
 
-screen_width = 1100
+screen_width = 1000
 screen_length = 500
 screen = pygame.display.set_mode((screen_width, screen_length))
 
@@ -18,29 +18,29 @@ bg = pygame.image.load("bg.png").convert()
 bg_width = bg.get_width()
 
 move = 0
-tiles = math.ceil(screen_width/bg_width) + 1
-print(tiles)
+#tiles = math.ceil(screen_width/bg_width) + 1
+#print(tiles)
 
-go = False
-go_back = False
+#go = False
+#go_back = False
 
 run = True
 
 # -------- Main Program Loop -----------
 while run:
 
-    clock.tick(fps)
-    for i in range(0, tiles):
-        screen.blit(bg, (i * bg_width + move, 0))
+    #clock.tick(fps)
+    #for i in range(0, tiles):
+        #screen.blit(bg, (i * bg_width + move, 0))
+    screen.blit(bg,(0,0))
+    #if abs(move) > bg_width:
+        #move = 0
 
-    if abs(move) > bg_width:
-        move = 0
+    #if go:
+        #move -=5
 
-    if go:
-        move -=5
-
-    if go_back:
-        move +=5
+    #if go_back:
+        #move +=5
 
     # --- Main event loop
     for event in pygame.event.get():# User did something
@@ -50,17 +50,17 @@ while run:
     #keys = pygame.key.get_pressed()
 
 
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
-            go = True
+        #if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+            #go = True
 
-        elif event.type == pygame.KEYUP and event.key == pygame.K_d:
-            go = False
+        #elif event.type == pygame.KEYUP and event.key == pygame.K_d:
+            #go = False
 
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-            go_back = True
+        #elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+            #go_back = True
 
-        elif event.type == pygame.KEYUP and event.key == pygame.K_a:
-            go_back = False
+        #elif event.type == pygame.KEYUP and event.key == pygame.K_a:
+            #go_back = False
 
 
 
