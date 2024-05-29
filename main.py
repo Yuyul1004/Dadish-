a6b34fa0c04207c378297b51d2954d4e0b17ea18
Yuyul1004 <y.lin2349@gmail.com>
2024-05-29 00:27:24 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pygame
 import math
-from dadish import Dadish
-
 
 
 pygame.init()
@@ -9,73 +7,50 @@ pygame.font.init()
 font = pygame.font.SysFont('Arial', 15)
 pygame.display.set_caption("Dadish")
 
-#player_image = pygame.image.load("DADISH_image.png")
-#clock = pygame.time.Clock()
-#fps = 50
-
 screen_width = 1000
-screen_length = 500
-screen = pygame.display.set_mode((screen_width, screen_length))
+screen_height = 500
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-# load image
+#Variables
+clock = pygame.time.Clock()
+game_speed = 150
+
+#Load images
 bg = pygame.image.load("bg.png").convert()
-#bg_width = bg.get_width()
+ground_image = pygame.image.load("ground.png")
+ground = ground_image.convert_alpha()
 
-move = 0
-#tiles = math.ceil(screen_width/bg_width) + 1
-#print(tiles)
 
-#go = False
-#go_back = False
-d = Dadish(100,300)
+# background scrolling
+bg_width = bg.get_width()
+bg_x = 0
+
+#ground scrolling
+ground_x = 0
+
+
 run = True
-
 # -------- Main Program Loop -----------
 while run:
-    keys = pygame.key.get_pressed()  # checking pressed keys
-    if keys[pygame.K_d]:
-        d.move_direction("right")
-
-    elif keys[pygame.K_a]:
-        d.move_direction("left")
-
-
-    #clock.tick(fps)
-    #for i in range(0, tiles):
-        #screen.blit(bg, (i * bg_width + move, 0))
-
-    #if abs(move) > bg_width:
-        #move = 0
-
-    #if go:
-        #move -=5
-
-    #if go_back:
-        #move +=5
 
     # --- Main event loop
     for event in pygame.event.get():# User did something
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
 
-    screen.blit(bg,(0,0))
-    screen.blit(d.image,d.rect)
-    #keys = pygame.key.get_pressed()
+    delta_time = clock.tick(60) / 1000
+    bg_x -= game_speed * delta_time
+    ground_x -= game_speed * delta_time
 
+    if bg_x < -bg_width:
+        bg_x = 0
+    screen.blit(bg, (bg_x, 0))
+    screen.blit(bg, (bg_x + bg_width, 0))
 
-        #if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
-            #go = True
-
-        #elif event.type == pygame.KEYUP and event.key == pygame.K_d:
-            #go = False
-
-        #elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-            #go_back = True
-
-        #elif event.type == pygame.KEYUP and event.key == pygame.K_a:
-            #go_back = False
-
-
+    if ground_x <= -screen_width:
+        ground_x = 0
+    screen.blit(ground, (ground_x, screen_height - ground.get_height()))
+    screen.blit(ground, (ground_x + screen_width, screen_height - ground.get_height()))
 
 
     pygame.display.update()
